406cac2c76bac68fffcb13877af9b64618fd6b8a
Pablo Lorenzatto <plorenzatto@buenosaires.gob.ar>
2015-08-05 11:18:45 -0300

    Esqueleto del modulo de analisis

---
+++ analisis/anomalyDetection.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Esta funcion implementa el detector de anomalias.
+Recibe como entrada 
+"""
+def detectAnomalies() :
+    pass
+
+
+def defineDetectionParams():
+    pass
