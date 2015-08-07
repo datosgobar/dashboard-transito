7d70ee0aa6128a297c53f97bbf12129a607d36dd
Pili Mayora <pili.mayora@gmail.com>
2015-08-07 16:44:37 -0300

    Separo la obtencion de raw data de teracode y el almacenamiento en la base de mysql

--- analisis/analisis.py
+++ analisis/analisis.py
@@ -127,15 +127,14 @@ def setupDB () :
 """
 Baja datos de nuevos de teracode y los guarda en la tabla "historical"
 """
-def updateDB(sensores, desde, hasta, step = datetime.timedelta(days=2)) : 
+def updateDB(data) : 
     conn = getDBConnection()
-    result = downloadData(sensores, step, desde, hasta)
     # parsear json
     Session = sessionmaker(bind=conn)
     session = Session()
     # loopear por cada corredor
     newrecords = False
-    for corredor in result:
+    for corredor in data:
         if not bool(corredor):
             continue
         for segmento in corredor["datos"]["data"]:
@@ -175,8 +174,9 @@ def executeLoop(desde, hasta) :
     sensores = [10,12,57, 53,51,49, 40, 43, 37,36, 21, 31,33,35, 13,14, 18,17,23, \
     24,25, 26,28, 30,32 ,45, 47, 38, 44, 48,48, 11,56, 54,55, 41, 22, 16,15, 19, 20, 10, 27,29, 34, 39, 42, 46, 50 ,52]
     
-    newrecords = updateDB(sensores, desde, hasta)
-    if newrecords : 
+    raw_data = downloadData(sensores, datetime.timedelta(days=2), desde, hasta)
+    has_new_records = updateDB(raw_data)
+    if has_new_records : 
         performAnomalyAnalysis(hasta)
 
 """
