99ad26d75d54892354a9f52e5d4e53204b81b1bf
Luis Uranga <luisuranga@buenosaires.gob.ar>
2015-08-10 16:17:54 -0300

    creo script en python qeu ejecuta dailyUpdate de analisis, y carpeta cron en openshift que llama mediante funcion bash al script

---
+++ analisis/dailyUpdate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import analisis
+import os
+
+
+def run():
+    """
+        sudo crontab -e
+        0 0 * * * /usr/bin/python2.7 /tu_home/tu_user/script-dashboardcivico/dailyUpdate.py
+    """
+    if os.environ.get('OPENSHIFT_PYTHON_DIR'):
+        # en caso de estarlo, activo entorno virtual y agarro las variables de
+        # entorno para ip y puerto
+        zvirtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'],
+                                'virtenv', 'bin', 'activate_this.py')
+        execfile(zvirtenv, dict(__file__=zvirtenv))
+
+    analisis.dailyUpdate()
+
+if __name__ == '__main__':
+    run()
