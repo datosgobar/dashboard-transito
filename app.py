723015bffb928f3d040860dd2f1086039f225f7e
Luis Uranga <luisuranga@buenosaires.gob.ar>
2015-08-10 12:42:18 -0300

    idento pep8 app.py y incorporo requests[security] en setup

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 import bottle
 import time
 
-from analisis import * 
+from analisis import *
 from bottle import error
 from socketio import socketio_manage
 from socketio.mixins import BroadcastMixin
@@ -19,7 +19,8 @@ app = bottle.Bottle()
 
 # inicio condicional para evaluar si estoy en openshift
 if os.environ.get('OPENSHIFT_PYTHON_DIR'):
-    #en caso de estarlo, activo entorno virtual y agarro las variables de entorno para ip y puerto
+    # en caso de estarlo, activo entorno virtual y agarro las variables de
+    # entorno para ip y puerto
     zvirtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'],
                             'virtenv', 'bin', 'activate_this.py')
     execfile(zvirtenv, dict(__file__=zvirtenv))
@@ -31,55 +32,66 @@ else:
     port = 8080
 
 # clase que hereda funcionalidades de socketio
+
+
 class dataSemaforos(BaseNamespace, BroadcastMixin):
 
     def init(self):
-      with open(os.path.abspath("analisis/template.json")) as templatecorredores:
-        template_buffer = buffer(templatecorredores.read())
-        self.template = json.loads(template_buffer.__str__())
+        with open(os.path.abspath("analisis/template.json")) as templatecorredores:
+            template_buffer = buffer(templatecorredores.read())
+            self.template = json.loads(template_buffer.__str__())
 
     def clean(self):
 
-      for key, value in self.template['corredores'].iteritems():
-        self.template['corredores'][key]['segmentos_provincia'] = []
-        self.template['corredores'][key]['segmentos_capital'] = []
+        for key, value in self.template['corredores'].iteritems():
+            self.template['corredores'][key]['segmentos_provincia'] = []
+            self.template['corredores'][key]['segmentos_capital'] = []
 
     # metodo que escucha on_<<CHANNELL>>(self, msg) enviado desde el front
     def on_receive(self, msg):
 
-      self.init()
+        self.init()
 
-      if msg:
-        
-        print "connect"
-        while True:
-          self.clean()
-          parserEmitData(self, self.template)
-          time.sleep(300)
+        if msg:
+
+            print "connect"
+            while True:
+                self.clean()
+                parserEmitData(self, self.template)
+                time.sleep(300)
 
     def recv_disconnect(self):
-      print "disconnect"
+        print "disconnect"
 
 # genero ruta / que envia template index
+
+
 @app.get('/')
 def root():
     return bottle.template('index')
 
 # path para datos estaticos en el front
+
+
 @app.get('/_static/<filepath:path>')
 def get_static(filepath):
     return bottle.static_file(filepath, root='./static/')
 
 # resuelve errores 404 y 500, en un decorador, para rutear a la funcion handler
+
+
 @error(404)
 @error(500)
 def handler_error(error):
     return 'Nothing here, sorry'
 
 # genero ruta /alertas para el manejo de socket.io, con xhr-polling y websocket
+
+
 @app.get('/socket.io/<path:path>')
 def socketio_service(path):
-    socketio_manage(bottle.request.environ, {'/alertas': dataSemaforos}, bottle.request)
+    socketio_manage(
+        bottle.request.environ, {'/alertas': dataSemaforos}, bottle.request)
 
 if __name__ == '__main__':
     print 'Listening on port {0} ip {1}'.format(ip, port)
