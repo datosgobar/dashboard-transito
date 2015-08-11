53ab307bd11c8647cc021d9a48e60c841114e320
Luis Uranga <luisuranga@buenosaires.gob.ar>
2015-08-11 14:43:38 -0300

    mejoro la la manera de ejecucion del script runUpdate

--- .openshift/cron/daily/runUpdate.sh
+++ .openshift/cron/daily/runUpdate.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python ${OPENSHIFT_REPO_DIR}analisis/dailyUpdate.py
-
-
+/var/lib/openshift/55bbc87dca988e695100000e/python/virtenv/bin/python ${OPENSHIFT_REPO_DIR}analisis/dailyUpdate.py >> ${OPENSHIFT_PYTHON_LOG_DIR}dailyUpdate.log 2>&1 &
