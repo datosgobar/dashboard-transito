950142b1dda6d7d6ef62aa7916da5fe21a48b480
Pablo Lorenzatto <plorenzatto@buenosaires.gob.ar>
2015-08-21 15:28:21 -0300

    Agregado codigo para suavizar la transicion entre franjas.

--- analisis/anomalyDetection.py
+++ analisis/anomalyDetection.py
@@ -6,14 +6,16 @@ import json
 import StringIO
 
 
+franjas = [
+    ("00:00:00", "07:00:00"),
+    ("07:00:00", "10:00:00"),
+    ("10:00:00", "17:00:00"),
+    ("17:00:00", "20:00:00"),
+    ("20:00:00", "23:59:00"),
+]
+
+
 def prepareDataFrame(df, timeadjust=None, doimputation=False):
-    franjas = [
-        ("00:00:00", "07:00:00"),
-        ("07:00:00", "10:00:00"),
-        ("10:00:00", "17:00:00"),
-        ("17:00:00", "20:00:00"),
-        ("20:00:00", "23:59:00"),
-    ]
     dfdaytype = pd.DataFrame([
         "workingday",
         "workingday",
@@ -69,6 +71,7 @@ def prepareDataFrame(df, timeadjust=None, doimputation=False):
     # Agregado de dia tipo de dia
     #df["weekday"] = df.date.apply(lambda e: e.weekday())
     df["weekday"] = df.date.dt.weekday
+    df["time"] = df.date.dt.time
     df["daytype"] = dfdaytype.loc[df.weekday].values[:, 0]
     #df.loc[df.weekday.isin([0, 1, 2, 3, 4]), "daytype"] = "workingday"
     #df.loc[df.weekday == 5, "daytype"] = "saturday"
@@ -77,8 +80,8 @@ def prepareDataFrame(df, timeadjust=None, doimputation=False):
     # Agregado de franjas horarias
     for (i, (begin, end)) in enumerate(franjas):
         df.ix[df.index.indexer_between_time(
-            start_time=begin, end_time=end), "franja"] = i
-    df["franja"] = df["franja"].astype(int)
+            start_time=begin, end_time=end), "franja"] = int(i)
+    #df["franja"] = df["franja"].astype(int)
     return df.reset_index(drop=True)
 
 """
@@ -127,8 +130,12 @@ def _detectAnomalies(detectparams, lastrecords):
     evalfield = "data"
     basefield = "mean"
     marginfield = "std"
+    # resultsdf = pd.merge(
+    # lastrecords, detectparams, on=["iddevice", "franja",
+    # "daytype"]).sort("date")
     resultsdf = pd.merge(
-        lastrecords, detectparams, on=["iddevice", "franja", "daytype"]).sort("date")
+        lastrecords, detectparams, on=["iddevice", "franja", "daytype", "time"]).sort("date")
+
     #anomalies = resultsdf[resultsdf[evalfield]>(resultsdf[basefield]+resultsdf[marginfield])]
     #resultsdf["threshold"] = resultsdf[basefield]
     resultsdf["threshold"] = resultsdf[basefield] + resultsdf[marginfield] * 2
@@ -150,9 +157,68 @@ def computeDetectionParams(lastmonthrecords):
 
 
 def _computeDetectionParams(lastmonthrecords):
-    output = lastmonthrecords.groupby(["iddevice", "franja", "daytype"])["data"].agg(
+    detectparams = lastmonthrecords.groupby(["iddevice", "franja", "daytype"])["data"].agg(
         {"mean": "mean", "std": "std"}).reset_index()
-    return output
+
+    timetable = pd.DataFrame(
+        index=pd.date_range("00:00", "23:59", freq="5min"))
+    for (i, (begin, end)) in enumerate(franjas):
+        timetable.ix[timetable.index.indexer_between_time(
+            start_time=begin, end_time=end), "franja"] = int(i)
+
+    timetable = timetable.reset_index()
+    timetable.columns = ["time", "franja"]
+    timetable["time"] = timetable.time.dt.time
+    resampled_detectparams = pd.merge(detectparams, timetable)
+
+    # Funcion de ventana para ensanchar margenes
+
+    def widdenSeries(s):
+        ret = pd.rolling_max(s, window=20, center=True)
+        ret = ret.fillna(method="ffill").fillna(method="bfill")
+        ret = pd.rolling_mean(ret, window=10, center=True)
+        ret = ret.fillna(method="ffill").fillna(method="bfill")
+        return ret
+
+    # resampled_detectparams = resampled_detectparams.set_index(
+    #    ["iddevice", "daytype"])
+    # newdetectparams_gp = resampled_detectparams.groupby(
+    #    level=["iddevice", "daytype"])
+    #newdetectparams = newdetectparams_gp["mean", "std"].transform(widdenSeries)
+    #newdetectparams["franja"] = resampled_detectparams["franja"]
+    #newdetectparams["time"] = resampled_detectparams["time"]
+    #newdetectparams = newdetectparams.reset_index()
+
+    resampled_detectparams.set_index(["iddevice", "daytype"], inplace=True)
+    for iddevice in detectparams.iddevice.unique():
+        # workdf = resampled_detectparams[
+        #    resampled_detectparams["iddevice"] == iddevice]
+        # workdf.loc[["workingday", "saturday", "sunday"], ["mean", "std"]] = widdenSeries(workdf.ix[
+        #    ["workingday", "workingday", "saturday", "sunday", "workingday"], ["mean", "std"]]).iloc[
+        #    len(workdf.ix["workingday"]): -len(workdf.ix["workingday"])]
+        # resampled_detectparams.iloc[["workingday", "saturday", "sunday"], ["mean", "std"]].loc[:, ] = widdenSeries(workdf.ix[
+        #    ["workingday", "workingday", "saturday", "sunday", "workingday"], ["mean", "std"]]).iloc[
+        #    len(workdf.ix["workingday"]): -len(workdf.ix["workingday"])]
+        # resampled_detectparams.loc[
+        #    resampled_detectparams["iddevice"] == iddevice].ix[["workingday", "saturday", "sunday"], ["mean", "std"]] = widdenSeries(workdf.ix[
+        #        ["workingday", "workingday", "saturday", "sunday", "workingday"], ["mean", "std"]]).iloc[
+        #    len(workdf.ix["workingday"]): -len(workdf.ix["workingday"])]
+        workdf = resampled_detectparams.loc[iddevice, :]
+        a = widdenSeries(workdf.ix[
+            ["workingday", "workingday", "saturday", "sunday", "workingday"], ["mean", "std"]]).iloc[
+            len(workdf.ix["workingday"]): -len(workdf.ix["workingday"])]
+        a["iddevice"] = iddevice
+        a = a.reset_index()
+        a.set_index(["iddevice", "daytype"], inplace=True)
+
+        resampled_detectparams.loc[(iddevice, "workingday"), "mean"] = a.loc[
+            (iddevice, "workingday"), "mean"]
+        resampled_detectparams.loc[(iddevice, "saturday"), "mean"] = a.loc[
+            (iddevice, "saturday"), "mean"]
+        resampled_detectparams.loc[(iddevice, "sunday"), "mean"] = a.loc[
+            (iddevice, "sunday"), "mean"]
+
+    return resampled_detectparams.reset_index()
 
 
 """
