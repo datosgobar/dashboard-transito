a75ad594a2ac7ea2749da8032c5076195c1da42b
Luis Uranga <luisuranga@buenosaires.gob.ar>
2015-08-31 16:41:40 -0300

    sumo _id en anomalia

--- analisis/analisis.py
+++ analisis/analisis.py
@@ -75,7 +75,7 @@ newdata = downloadData (
 """
 
 
-def downloadData(sensor_ids, step, download_startdate, download_enddate, outfn=None, token="superadmin.", pool_len=48):
+def downloadData(sensor_ids, step, download_startdate, download_enddate, outfn=None, token="superadmin.", pool_len=5):
 
     # vsensids = virtsens["id_sensor"].unique()
     urltpl = "https://apisensores.buenosaires.gob.ar/api/data/%s?token=%s&fecha_desde=%s&fecha_hasta=%s"
@@ -97,16 +97,16 @@ def downloadData(sensor_ids, step, download_startdate, download_enddate, outfn=N
         start += step
 
     """cambiar funcion map por api_sensores_fake"""
-    alldata = map(api_sensores_fake, urls)
-    # pool = multiprocessing.Pool(pool_len)
-    # alldata = pool.map(getData, urls)
-    # pool.close()
-    # pool.terminate()
-    # pool.join()
-    # if outfn != None:
-    #     outf = open(outfn, "wb")
-    #     json.dump(alldata, outf)
-    #     outf.close()
+    #alldata = map(api_sensores_fake, urls)
+    pool = multiprocessing.Pool(pool_len)
+    alldata = pool.map(getData, urls)
+    pool.close()
+    pool.terminate()
+    pool.join()
+    if outfn != None:
+        outf = open(outfn, "wb")
+        json.dump(alldata, outf)
+        outf.close()
 
     return alldata
 
@@ -188,6 +188,7 @@ Filtro los registros para no duplicar datos en la base de datos
 
 
 def filterDuplicateRecords(data, desde=None, hasta=None):
+    # pdb.set_trace()
     # "Removing duplicates"
     conn = getDBConnection()
     # parsear json
@@ -537,9 +538,9 @@ def performAnomalyAnalysis(ahora=None):
 
 def downloadAndLoadLastMonth():
 
+    #sensores = [10, 12]
     sensores = [10, 12, 57, 53, 51, 49, 40, 43, 37, 36, 21, 31, 33, 35, 13, 14, 18, 17, 23, 24, 25, 26, 28, 30,
-                32, 45, 47, 38, 44, 48, 48, 11, 56, 54, 55, 41, 22, 16, 15, 19, 20, 10,
-                27, 29, 34, 39, 42, 46, 50, 52]
+                32, 45, 47, 38, 44, 48, 48, 11, 56, 54, 55, 41, 22, 16, 15, 19, 20, 10, 27, 29, 34, 39, 42, 46, 50, 52]
 
     hasta = datetime.datetime.now()
     desde = hasta - datetime.timedelta(days=28)
