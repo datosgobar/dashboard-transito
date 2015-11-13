f1b20c3992759f76f50cb7409325de8fe0a1a94c
Luis Uranga <luisuranga@buenosaires.gob.ar>
2015-11-13 15:00:41 -0300

    saco dependencia que no utilizo, actualizo setup yreadme

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     url='http://git.gcba.gob.ar/labgcba/dashboard-operativo-transito.git',
     install_requires=['bottle==0.12.8', 'gevent==1.1b1', 'gevent-socketio==0.3.5', 'requests==2.7.0', 'requests[security]',
                       'MySQL-python==1.2.3', 'sqlalchemy==1.0.8', 'pandas==0.16.2', 'dateutil==2.4.2', 'sqlalchemy-migrate',
-                      'bottle-cork==0.11.1']
+                      'bottle-cork==0.11.1', 'seaborn==0.6.0']
 )
